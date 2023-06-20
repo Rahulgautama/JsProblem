eb38b0ae28605585aa8cd4f01d01fc14c2d24f72
Rahulgautama <rahul8874950727@gmail.com>
2023-06-20 22:09:34 +0530

    Calculate Emp wage for a month

--- EmpWage.js
+++ EmpWage.js
@@ -14,8 +14,11 @@ switch (empCheck) {
     	return empHrs = 0;
 	}
 }
+const NUM_OF_WORKING_DAYS = 20;
 let empHrs = 0;
-empCheck = Math.floor(Math.random() * 10) % 3; 
-empHrs=getWorkingHours(empCheck);
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Emp Wage: " + empWage);
+for (let day = 0; day< NUM_OF_WORKING_DAYS; day++) {
+let empCheck = Math.floor(Math.random() * 10) % 3;
+empHrs += getWorkingHours (empCheck);
+} 
+let empWage = empHrs*WAGE_PER_HOUR;
+console.log("Total Hrs: "+empHrs+ " Emp Wage: "+empWage);
