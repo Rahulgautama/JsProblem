a5b143abcb3e06aa1eb3965858eb1ad1618b8d00
Rahulgautama <rahul8874950727@gmail.com>
2023-06-20 22:11:28 +0530

    Calculate Emp wage for a month where condition

--- EmpWage.js
+++ EmpWage.js
@@ -1,3 +1,6 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
 const IS_PART_TIME = 1; 
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
@@ -14,11 +17,14 @@ switch (empCheck) {
     	return empHrs = 0;
 	}
 }
+const MAX_HRS_IN_MONTH = 160;
 const NUM_OF_WORKING_DAYS = 20;
-let empHrs = 0;
-for (let day = 0; day< NUM_OF_WORKING_DAYS; day++) {
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+totalWorkingDays++;
 let empCheck = Math.floor(Math.random() * 10) % 3;
-empHrs += getWorkingHours (empCheck);
-} 
-let empWage = empHrs*WAGE_PER_HOUR;
-console.log("Total Hrs: "+empHrs+ " Emp Wage: "+empWage);
+totalEmpHrs += getWorkingHours(empCheck);
+}
+let empwage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("UC5 Total Days: "+totalWorkingDays +" Total Hrs: "+totalEmpHrs +" Emp Wage: "+ empwage);
