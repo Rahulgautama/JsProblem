5172c83106157957b27c5221d46568f228ba16f6
Rahulgautama <rahul8874950727@gmail.com>
2023-06-20 22:07:11 +0530

    Refactor code Calculate Emp wage using function

--- EmpWage.js
+++ EmpWage.js
@@ -3,17 +3,19 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8; 
 const WAGE_PER_HOUR = 20;
-let empHrs = 0;
-empCheck = Math.floor(Math.random() * 10) % 3; 
+
+function getWorkingHours(empCheck){
 switch (empCheck) { 
     case IS_PART_TIME: 
-        empHrs=PART_TIME_HOURS; 
-        break; 
+        return empHrs=PART_TIME_HOURS;
     case IS_FULL_TIME:
-        empHrs=FULL_TIME_HOURS;
-        break;
+        return empHrs=FULL_TIME_HOURS;        
     default:
-    empHrs = 0;
+    	return empHrs = 0;
+	}
 }
+let empHrs = 0;
+empCheck = Math.floor(Math.random() * 10) % 3; 
+empHrs=getWorkingHours(empCheck);
 let empWage = empHrs * WAGE_PER_HOUR;
 console.log("Emp Wage: " + empWage);
